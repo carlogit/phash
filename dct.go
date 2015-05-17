6cad2e7d54c39aa2cbcd6aef2072cd0109b6a728
carlo <cvargasdev@gmail.com>
2015-05-16 21:54:23 -0400

    add some tests

--- dct.go
+++ dct.go
@@ -36,22 +36,24 @@ func getInitDCTTable(img image.Image) [][]float64 {
 }
 
 func getBlue(img image.Image, x int, y int) float64 {
-	r, _, _, _ := img.At(x, y).RGBA()
-	return float64(r & 0xff)
+	_, _, b, _ := img.At(x, y).RGBA()
+	return float64(b & 0xff)
 }
 
 func getDCTValues(img image.Image) [][]float64 {
 	vals := getInitDCTTable(img)
-	N := size
 
-	F := make([][]float64, N)
-	for u := 0; u < N; u++ {
-		F[u] = make([]float64, N)
-		for v := 0; v < N; v++ {
+	xSize := img.Bounds().Max.X
+	ySize := img.Bounds().Max.Y
+
+	F := make([][]float64, xSize)
+	for u := 0; u < xSize; u++ {
+		F[u] = make([]float64, ySize)
+		for v := 0; v < ySize; v++ {
 			sum := float64(0.0)
-			for i := 0; i < N; i++ {
-				for j := 0; j < N; j++ {
-					valor := math.Cos((2.0*float64(i)+1.0)/(2.0*float64(N))*float64(u)*math.Pi) * math.Cos((2.0*float64(j)+1.0)/(2.0*float64(N))*float64(v)*math.Pi) * (vals[i][j])
+			for i := 0; i < xSize; i++ {
+				for j := 0; j < ySize; j++ {
+					valor := math.Cos((2.0*float64(i)+1.0)/(2.0*float64(xSize))*float64(u)*math.Pi) * math.Cos((2.0*float64(j)+1.0)/(2.0*float64(ySize))*float64(v)*math.Pi) * (vals[i][j])
 					sum += valor
 				}
 			}
@@ -59,5 +61,6 @@ func getDCTValues(img image.Image) [][]float64 {
 			F[u][v] = sum
 		}
 	}
+
 	return F
 }
--- phash.go
+++ phash.go
@@ -11,23 +11,43 @@ const (
 	smallerSize int = 8
 )
 
-func GetHash(reader io.Reader) string {
-	img, err := imaging.Decode(reader)
+func GetHash(reader io.Reader) (string, error) {
+	image, err := imaging.Decode(reader)
 
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	img = imaging.Resize(img, size, size, imaging.Lanczos)
-	img = imaging.Grayscale(img)
+	image = imaging.Resize(image, size, size, imaging.Lanczos)
+	image = imaging.Grayscale(image)
 
-	dctValues := getDCTValues(img)
+	dctValues := getDCTValues(image)
 
-	return buildHash(dctValues)
+	return buildHash(dctValues), nil
+}
+
+func buildHash(dctValues [][]float64) string {
+	dctMeanValue := calculateMeanDCTValue(dctValues)	
+	
+	var hash string
+
+	for x := 0; x < smallerSize; x++ {
+		for y := 0; y < smallerSize; y++ {
+			if x != 0 && y != 0 {
+				if dctValues[x][y] > dctMeanValue {
+					hash += "1"
+				} else {
+					hash += "0"
+				}
+			}
+		}
+	}	
+	
+	return hash
 }
 
 func calculateMeanDCTValue(dctValues [][]float64) float64 {
-	total := float64(0)
+	var total float64
 
 	for x := 0; x < smallerSize; x++ {
 		for y := 0; y < smallerSize; y++ {
@@ -42,25 +62,6 @@ func calculateMeanDCTValue(dctValues [][]float64) float64 {
 	return avg
 }
 
-func buildHash(dctValues [][]float64) string {
-	avg := calculateMeanDCTValue(dctValues)	
-	
-	hash := ""
-
-	for x := 0; x < smallerSize; x++ {
-		for y := 0; y < smallerSize; y++ {
-			if x != 0 && y != 0 {
-				if dctValues[x][y] > avg {
-					hash += "1"
-				} else {
-					hash += "0"
-				}
-			}
-		}
-	}	
-	
-	return hash
-}
 
 func GetDistance(hash1, hash2 string) int {
 	distance := 0
